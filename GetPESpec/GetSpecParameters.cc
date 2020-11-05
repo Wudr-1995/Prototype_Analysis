022ceb53834eb59e1ef3cb0792727b751ce6527b
wudr-1995 <wudr@ihep.ac.cn>
2020-11-05 16:19:16 +0800

    update

--- GetPESpec/GetSpecParameters.cc
+++ GetPESpec/GetSpecParameters.cc
@@ -19,7 +19,7 @@ double CalibGain(TH1F* InSpec) {
 	}
 	int HalWidth = pedestal - start;
 	int end = pedestal + HalWidth * 0.9;
-	TF1* GausFunc = new TF1("gaus", "gaus", start * 0.02, end * 0.02);
+	TF1* GausFunc = new TF1("gaus", "gaus", start * 0.02 - 50, end * 0.02 - 50);
 	InSpec->Fit(GausFunc, "R");
 	Double_t pars[3];
 	GausFunc->GetParameters(pars);
@@ -27,7 +27,7 @@ double CalibGain(TH1F* InSpec) {
 	TH1D* test = new TH1D("test", "test", 10000, -50, 150);
 	for (int i = 0; i < 10000; i ++) {
 		Double_t x[1];
-		x[0] = i * 25;
+		x[0] = i * 0.02 - 50;
 		Double_t Spe = InSpec->GetBinContent(i) - mygaus(x, pars) < 0 ? 0 : InSpec->GetBinContent(i) - mygaus(x, pars);
 		QOut->SetBinContent(i, Spe);
 		// cout << "Debug: " << InSpec->GetBinContent(i) << "\t" << mygaus(x, pars) << endl;
@@ -38,21 +38,20 @@ double CalibGain(TH1F* InSpec) {
 	int Spe_peak = QOut->GetBinContent(Spe_pos);
 	if (Spe_pos < pedestal + HalWidth) {
 		for (int i = Spe_pos; i > 0; i --) {
-			if (QOut->GetBinContent(i) < Spe_peak * 0.1) {
+			if (QOut->GetBinContent(i) < Spe_peak * 0.3) {
 				start = i;
 				break;
 			}
 		}
 		HalWidth = Spe_pos - start;
 		end = Spe_pos + HalWidth;
-		cout << start << "\t" << end << endl;
-		TF1* tmp = new TF1("tmp_gaus", "gaus", start * 0.02, end * 0.02);
+		TF1* tmp = new TF1("tmp_gaus", "gaus", start * 0.02 - 50, end * 0.02 - 50);
 		QOut->Fit(tmp, "R");
 		Double_t tmp_pars[3];
 		tmp->GetParameters(tmp_pars);
 		for (int i = 0; i < 10000; i ++) {
 			Double_t x[1];
-			x[0] = i * 25;
+			x[0] = i * 0.02 - 50;
 			Double_t Spe = QOut->GetBinContent(i) - mygaus(x, tmp_pars);
 			Spe = Spe < 0 ? 0 : Spe;
 			QOut->SetBinContent(i, Spe);
@@ -61,20 +60,20 @@ double CalibGain(TH1F* InSpec) {
 	Spe_pos = QOut->GetMaximumBin();
 	Spe_peak = QOut->GetBinContent(Spe_pos);
 	for (int i = Spe_pos; i < 10000; i ++) {
-		if (QOut->GetBinContent(i) < Spe_peak * 0.2) {
+		if (QOut->GetBinContent(i) < Spe_peak * 0.3) {
 			end = i;
 			break;
 		}
 	}
 	HalWidth = end - Spe_pos;
 	start = Spe_pos - HalWidth * 0.5;
-	TF1* SpeFunc = new TF1("Spe_gaus", "gaus", start * 0.02, end * 0.02);
+	TF1* SpeFunc = new TF1("Spe_gaus", "gaus", start * 0.02 - 50, end * 0.02 - 50);
 	QOut->Fit(SpeFunc, "R");
 	Double_t SpePars[3];
 	SpeFunc->GetParameters(SpePars);
 	const Double_t* SpeParsEr = SpeFunc->GetParErrors();
-	Spe_pos = SpePars[1] / 25;
-	TH1D* tmp = new TH1D("tmp", "tmp", Spe_pos - pedestal, 0, (Spe_pos - pedestal) * 0.02);
+	Spe_pos = SpePars[1] / 0.02;
+	TH1D* tmp = new TH1D("tmp", "tmp", Spe_pos - pedestal, -50, (Spe_pos - pedestal) * 0.02 - 50);
 	for (int i = pedestal; i < Spe_pos; i ++) {
 		tmp->SetBinContent(i - pedestal + 1, InSpec->GetBinContent(i));
 	}
@@ -83,7 +82,7 @@ double CalibGain(TH1F* InSpec) {
 	// double peakV = SpePars[0];
 	// double peakEr = SpeParsEr[0];
 	Double_t valleyEr = tmp->GetBinError(tmp->GetMinimumBin());
-	double Gain = (SpePars[1] - pars[1]) * 0.01 / 1.6;
+	double Gain = (SpePars[1] - pars[1]) / 0.16;
 	// double GainEr = (SpeParsEr[1] + PedParsEr[1]) * 0.01 / 1.6;
 	// double PV = peakV / valley;
 	// double PVEr = peakEr / valley + valleyEr * PV / valley;
